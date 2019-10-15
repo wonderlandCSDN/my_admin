19edc73597cdd24a479b27542a29491b09a94952
luoyanfei <luoyanfei@capol.cn>
2019-10-15 20:13:56 +0800

    执行脚本配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,4 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
+
+    /* 某某 */
+    /* BASE_API_ROOT: '"http://主机号:端口号"',
+    JWT_API_ROOT: '"http://主机号:端口号"', */
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,3 +2,33 @@
 module.exports = {
   NODE_ENV: '"production"'
 }
+
+
+// 获取当前正在运行的脚本名称
+// npm 提供一个npm_lifecycle_event变量，返回当前正在运行的脚本名称，可以配合顺序钩子使用
+
+
+/* let obj = {};
+const target = process.env.npm_lifecycle_event;
+if (target == 'test') {
+  obj = {
+      NODE_ENV: '"production"',
+      BASE_API_ROOT: '"http://主机号:端口"',
+      BASE_AUTO_LOGIN:'"http://主机号:端口"',
+      BASE_UNION_LOGIN:'"http://主机号:端口/#/login"'
+  }
+}else if (target == 'development') {
+  obj = {
+    NODE_ENV: '"production"',
+    BASE_API_ROOT: '"http://主机号:端口"',
+    BASE_AUTO_LOGIN:'"http://主机号:端口"',
+    BASE_UNION_LOGIN:'"http://主机号:端口/#/login"'
+  }
+}else{
+  obj = {
+    NODE_ENV: '"production"',
+    BASE_API_ROOT: '"http://主机号:端口"',
+    BASE_AUTO_LOGIN:'"http://主机号:端口"',
+    BASE_UNION_LOGIN:'"http://主机号:端口/#/login"'
+  }
+} */
